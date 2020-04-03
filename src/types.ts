acfcea3623609b26065a630b32573d34198166c3
skt-t1-byungi <tiniwz@gmail.com>
2020-04-03 21:43:39 +0900

    wip

--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,5 @@ export type OnFulfilled<T, R> = (value: T) => R|PromiseLike<R>
 export type OnRejected<T = never> = (reason: any) => T|PromiseLike<T>
 export type GenFunction<T> = (signal: AbortSignal) => Generator<any, T>
 export type AsyncFunction<R> = (...args: any) => PromiseLike<R>
-export type PromisesHandler<T, R> = (promises: PromiseLike<T>) => PromiseLike<R>
 export type AicoOptions = {signal?: AbortSignal; AbortController?: new() => AbortController}
+export type AsyncResult<F extends AsyncFunction<any>> = F extends AsyncFunction<infer R> ? R : never
