92999407f9f8eec0015dd28b47784a8ccfeb7b02
skt-t1-byungi <tiniwz@gmail.com>
2020-04-04 09:13:56 +0900

    wip

--- src/abortify.ts
+++ src/abortify.ts
@@ -1,7 +1,7 @@
 import AbortInCoroutines from './AbortInCoroutines'
 
-export default function <T, R> (fn: (values: Array<T|PromiseLike<T>>) => PromiseLike<R>) {
-    return (values: Array<T|PromiseLike<T>>) => new AbortInCoroutines<R>(function * (signal) {
+export default function <V, R> (fn: (values: Array<V|PromiseLike<V>>) => PromiseLike<R>) {
+    return (values: Array<V|PromiseLike<V>>) => new AbortInCoroutines<R>(function * (signal) {
         let isErr = false
         try {
             return (yield fn(values).then(
--- src/race.ts
+++ src/race.ts
@@ -2,5 +2,5 @@ import abortify from './abortify'
 import AbortInCoroutines from './AbortInCoroutines'
 
 export default abortify(Promise.race) as {
-    <T>(values: readonly T[]): AbortInCoroutines<T extends PromiseLike<infer U> ? U : T>;
+    <T>(values: T[]): AbortInCoroutines<T extends PromiseLike<infer U> ? U : T>;
 }
